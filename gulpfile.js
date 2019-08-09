d70bd3eeb182fc60597491de456610a3815b935a
Artur Trifonov <artur@uniteddesign.ru>
2019-08-09 21:17:48 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify-es').default,
+    concat = require('gulp-concat'),
+    include = require("gulp-include"),
+    livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+    return gulp.src('src/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        // .pipe(cssmin())
+        .pipe(gulp.dest('dist/static/css'))
+        .pipe(livereload());
+});
+gulp.task('html', function () {
+    return gulp.src('**/*.html').pipe(livereload());
+});
+gulp.task('js', function() {
+    return gulp.src('src/js/scripts.js')
+        .pipe(include())
+        .pipe(uglify())
+        .on('error', console.log)
+        .pipe(gulp.dest('dist/static/js'))
+        .pipe(livereload());
+});
+
+gulp.task('default', function () {
+    livereload.listen();
+    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('**/*.html', gulp.series('html'));
+    gulp.watch('src/js/**/*.js', gulp.series('js'));
+});
